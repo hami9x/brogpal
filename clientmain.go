5a6307e4b51d65b9b7814a1676e0a8f5a6e115c3
phaikawl <phaikawl@gmail.com>
2014-05-30 18:41:24 +0700

    Add some basic client things, add a handler to get auth token on user page.

--- clientmain.go
+++ clientmain.go
@@ -2,11 +2,16 @@ package main
 
 import (
 	wd "github.com/phaikawl/wade"
+	"github.com/phaikawl/wade/services/http"
 )
 
 type UserInfo struct {
 	Name string
 	Age  int
+}
+
+type AuthedStat struct {
+	AuthGened bool
 }
 
 func main() {
@@ -15,4 +20,19 @@ func main() {
 		Name: "Hai Thanh Nguyen",
 		Age:  18,
 	})
+	wade.RegisterPageHandler("login_handler", func() interface{} {
+		req := http.Service().NewRequest(http.MethodGet, "/auth")
+		promise := wd.NewPromise(&AuthedStat{false}, req.DoAsync())
+		promise.OnSuccess(func(r *http.Response) wd.ModelUpdater {
+			return func(as *AuthedStat) {
+				as.AuthGened = true
+			}
+		})
+		return promise.Model()
+	})
+
+	http.Service().AddHttpInterceptor(func(req *http.Request) {
+	})
+
+	wade.Start()
 }
